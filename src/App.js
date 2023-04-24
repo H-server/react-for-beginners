29fc01efc0daea88b7e7aef7619da4121a12c3ac
ksnghyk <hani103201@naver.com>
2023-04-24 20:43:00 +0900

    movie 웹 강의 끝!

--- src/App.js
+++ src/App.js
@@ -1,48 +1,17 @@
-import { useState, useEffect } from "react";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
-function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [usd, setUsd] = useState();
-  const [coin, setCoin] = useState();
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-        setCoin(json[0].quotes.USD.price);
-      });
-  }, []);
-  const onChange = (event) => {
-    setCoin(event.target.value);
-  }
-  const onSubmit = (event) => {
-    event.preventDefault();
-    setUsd(event.target.value);
-  }
-  return (
-    <div>
-      <h1>Coin Converter ({coins.length})</h1>
-      <hr />
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select onChange={onChange}>
-          {coins.map((coin) => (
-            <option value={coin.quotes.USD.price}>
-              {coin.name} {coin.symbol}: {coin.quotes.USD.price.toFixed(3)} USD
-            </option>
-          ))}
-        </select>
-      )}
-        <input type="number" placeholder="Write your USD" value={usd} onChange={onSubmit} />
-        <br />
-      <div>
-        {usd ? usd : 0} USD = {(usd/coin).toFixed(3) ? (usd/coin).toFixed(5) : 0} COIN
-      </div>
-    </div>
-  );
-}
+import { createBrowserRouter } from "react-router-dom";
+
+const App = createBrowserRouter([
+{
+path: "/",
+element: <Home />,
+},
+{
+path: "/movie/:id",
+element: <Detail /> ,
+},
+]);
 
 export default App;
